ff6fb9fc9cfd8bf9e7dfd05568975f3608cac1a2
heunseoRyu <120763372+heunseoRyu@users.noreply.github.com>
2023-05-12 17:53:36 +0900

    1271

--- fighting.py
+++ fighting.py
@@ -1,4 +1,19 @@
-n,m = map(int,input().split())
+n,m =map(int,input().split())
 
-print(n//m)
-print(n%m)
+a = []
+b = []
+
+for i in range(n):
+    temp = list(map(int,input().split()))
+    a.append(temp)
+
+for i in range(n):
+    temp = list(map(int,input().split()))
+    b.append(temp)
+
+for i in range(n):
+    for j in range(m):
+        print(a[i][j]+b[i][j],end=' ')
+    print()
+
+
